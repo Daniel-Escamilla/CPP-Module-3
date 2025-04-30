6a50896ecfdc0dde7e1637868ccffb36bb3d1c8c
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-04-30 21:24:51 +0200

    Add include header file for ClapTrap and ScavTrap with necessary guards

---
+++ ex01/include/include.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 20:13:32 by descamil          #+#    #+#             */
+/*   Updated: 2025/04/30 21:18:11 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_HPP
+# define INCLUDE_HPP
+
+# include <iostream>
+# include <string>
+
+# include "ClapTrap.hpp"
+# include "ScavTrap.hpp"
+
+#endif
