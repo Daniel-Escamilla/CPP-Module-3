a2f75487222fc61a72c24b9230e9ae24be157202
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-01 21:32:28 +0200

    Add ScavTrap class header file

---
+++ ex03/include/ScavTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 21:17:18 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/01 20:37:13 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : virtual public ClapTrap
+{
+	public:
+		ScavTrap(std::string name);
+		~ScavTrap();
+		void	guardGate();
+		void	attack(const std::string& target);
+		
+};
+
+#endif
