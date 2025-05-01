33d4934dc7a2aa14f18a01dadbfd4d56794303f8
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-01 20:14:30 +0200

    Add FragTrap class implementation with constructor, destructor, and high-five method

---
+++ ex02/src/FragTrap.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 19:50:24 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/01 20:05:42 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/FragTrap.hpp"
+
+FragTrap::FragTrap(std::string name)
+	: ClapTrap(name)
+{
+	setHitPoints(100);
+	setEnergyPoints(100);
+	setAttackDamage(30);
+	std::cout << "FragTrap " << getName() << " created" << std::endl;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "FragTrap " << getName() << " destroyed" << std::endl;
+}
+
+void	FragTrap::highFivesGuys(void)
+{
+    std::cout << "FragTrap " << getName() << " requests a high-five!" << std::endl;
+}
