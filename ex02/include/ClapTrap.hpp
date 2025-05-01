6f0209db6b0fea66cffd013057d94e51c45e5521
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-01 20:14:20 +0200

    Add ClapTrap class header file with member function declarations

---
+++ ex02/include/ClapTrap.hpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 20:13:20 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/01 19:35:42 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+# include <iostream>
+
+class ClapTrap
+{
+	private:
+		std::string	_name;
+		int			_hitPoints;
+		int			_energyPoints;
+		int			_attackDamage;
+	public:
+		ClapTrap(std::string name);
+		~ClapTrap();
+
+		std::string	getName() const;
+		int			getEnergyPoints() const;
+		int			getHitPoints() const;
+		int			getAttackDamage() const;
+
+		void		setEnergyPoints(int points);
+		void		setHitPoints(int points);
+		void		setAttackDamage(int points);
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+};
+
+#endif
