de6347f083b24cd37e8246b0b8a5c8b7b2ca3c3f
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-01 19:45:34 +0200

    Update ScavTrap.hpp to correct include directive and add member function declarations

--- ex01/include/ScavTrap.hpp
+++ ex01/include/ScavTrap.hpp
@@ -6,20 +6,22 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 21:17:18 by descamil          #+#    #+#             */
-/*   Updated: 2025/04/30 21:20:47 by descamil         ###   ########.fr       */
+/*   Updated: 2025/05/01 19:29:52 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
-# include "include.hpp"
+# include "ClapTrap.hpp"
 
 class ScavTrap : public ClapTrap
 {
 	public:
 		ScavTrap(std::string name);
 		~ScavTrap();
+		void	guardGate();
+		void	attack(const std::string& target);
 		
 };
 
