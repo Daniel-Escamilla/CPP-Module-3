55be5eee40f2171c65a044bd0855da64d55da781
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-01 21:32:25 +0200

    Add FragTrap class header file

---
+++ ex03/include/FragTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 19:47:22 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/01 20:37:23 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include "ScavTrap.hpp"
+
+class FragTrap : virtual public ClapTrap
+{
+	public:
+		FragTrap(std::string name);
+		~FragTrap();
+		void	highFivesGuys(void);
+};
+
+#endif
