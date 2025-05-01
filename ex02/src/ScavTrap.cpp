f897abec8fe79cd7c0b0b116b41c78f2796dac89
Daniel-Escamilla <daniel.escamilla.bq@gmail.com>
2025-05-01 20:14:41 +0200

    Add ScavTrap class implementation with constructor, destructor, guardGate, and attack methods

---
+++ ex02/src/ScavTrap.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/30 21:16:35 by descamil          #+#    #+#             */
+/*   Updated: 2025/05/01 20:06:01 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/FragTrap.hpp"
+
+ScavTrap::ScavTrap(std::string name)
+	: ClapTrap(name)
+{
+	setHitPoints(100);
+	setEnergyPoints(50);
+	setAttackDamage(20);
+	std::cout << "ScavTrap " << getName() << " created" << std::endl;	
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout << "ScavTrap " << getName() << " destroyed" << std::endl;
+}
+
+void	ScavTrap::guardGate()
+{
+	std::cout << "ScavTrap is now in Gate keeper mode" << std::endl;
+}
+
+void	ScavTrap::attack(const std::string& target)
+{
+	if (getHitPoints() > 0 && getEnergyPoints() > 0)
+	{
+		setEnergyPoints(getEnergyPoints() - 1);
+		std::cout << "ScavTrap " << getName() << " attacks " << target
+		<< ", causing " << getAttackDamage() << " points of damage" << std::endl;
+	}
+	else if (getEnergyPoints() == 0)
+		std::cout << "Not enough energy points" << std::endl;
+	else if (getHitPoints() == 0)
+		std::cout << "Not enough hit points" << std::endl;
+}
